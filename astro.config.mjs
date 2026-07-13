5f8b85f5c3ed72bce729e5934248737d21f2cbc3
2bconsult31 <2bconsult31@gmail.com>
2026-07-13 16:57:17 +0200

    Push sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://survivalix.fr',
+  site: 'https://www.survivalix.fr',
   integrations: [sitemap()],
 });
